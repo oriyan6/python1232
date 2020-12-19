51709b677058a6c2b22f61f6751352c988327c78
oriyan6 <oriyan612@gmail.com>
2020-12-19 21:18:03 +0200

    Initial commit

---
+++ a.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter the first number"))
+num2 = int(input("enter the second number"))
+num4 = int(input("enter the third number"))
+num5 = int(input("enter the fourth number"))
+average = (num1 + num2 + num4 + num5)/4
+print(average)
